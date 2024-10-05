85ecec8b15360d26864bd0180db2bba99a7dc117
viciious <vluchits@gmail.com>
2024-10-05 23:22:22 +0300

    Add safety guards

--- r_phase3.c
+++ r_phase3.c
@@ -86,6 +86,9 @@ static void R_PrepMobj(mobj_t *thing)
       flip = true;
    }
 
+   if (lump < firstsprite || lump >= firstsprite + numsprites)
+      return;
+
    patch = W_POINTLUMPNUM(lump);
    xscale = FixedDiv(PROJECTION, tz);
    gzt = thing->z - vd->viewz;
@@ -203,6 +206,9 @@ static void R_PrepPSprite(pspdef_t *psp)
    sprlump  = &spritelumps[sprframe->lump];
    lump     = sprlump[0];
    patch    = W_POINTLUMPNUM(lump);
+
+   if (lump < firstsprite || lump >= firstsprite + numsprites)
+      return;
 
    xscale = weaponXScale;
    center = centerXFrac - 160 * weaponXScale;
